fa3c72b71ac8e5a9d7c6690dfd731eea80c3e93b
AlvaroCoder <papux45@gmail.com>
2022-10-05 10:54:36 -0500

    really deploy

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const routesApi = require("./routes/api");
+const path = require('path');
 const PORT =  process.env.PORT || 8084 ;
 app.use(cors());
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+app.get("/",(req, res)=>{
+    const html_file = path.join(__dirname, '/pages/index.html')
+    res.sendFile(html_file);
+    
+});
+app.get("*",(req, res)=>{
+    res.sendFile(path.join(__dirname, '/pages/lost.html'))
+})
 app.use("/database",routesApi);
 
 app.listen(PORT, (e)=>{
